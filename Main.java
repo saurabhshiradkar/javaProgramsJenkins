441e2810c1449db48dce904b1454f43ee727d28b
Saurabh Shiradkar <72598771+saurabhshiradkar@users.noreply.github.com>
2024-03-08 10:23:15 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is out");
+        System.out.println("Out Put From Java aProgram");
+        System.out.println(new Date());
+
+    }
+}
